9f45242ecba6caa8b02842ec4035b4af754435d8
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-02-11 02:03:23 +0200

    Added the logic

--- src/semafor/Semafor.java
+++ src/semafor/Semafor.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package semafor;
+// Se scrie o clasa Semafor. Metoda main() genereaza in mod aleator culoarea semaforului
+//(rosu, galben sau verde) si o afiseaza pe ecran
 
-/**
- *
- * @author macbookpro
- */
 public class Semafor {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a = genereazaNumar(1, 3);
+        
+        if (a == 1) {
+            System.out.println("Culoarea semaforului este VERDE");
+        } else if (a == 2) {
+            System.out.println("Culoarea semaforului este GALBENA");
+        } else {
+            System.out.println("Culoarea semaforului este ROSIE");
+        }
+    } 
+    public static int genereazaNumar(int min, int max){
+        return (int)(Math.random() * ((max - min) + 1)) + min;
     }
-    
 }
